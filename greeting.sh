6514b04f12e69ec357799e1d7f58b89e44c581b9
stephen smithstone <ssmithstone@me.com>
2024-04-10 15:29:13 +1000

    Add current date and detailed date info to greeting.sh
    This commit introduces a feature to the greeting.sh script that displays not only a personalized greeting but also the current date and detailed information about the current date. This includes the hour, minute, second, day of the week, and a formatted date string.

--- greeting.sh
+++ greeting.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
 name="Luke Skywalker"
+date_now=$(date)
 echo "Hello, ${name}!"
 printf "Hello %s!\n" "${name}"
+echo "Today's date: ${date_now}"
+echo "Today's date as Epoc value: $(date +%s)"
+hour=$(date +%H)
+minute=$(date +%M)
+seconds=$(date +%S)
+day_of_week=$(date +%A)
+day_of_week_int=$(date +%u)
+formatted_date=$(date +%d/%m/%y)
+date_details="Date Details:
+Hour: ${hour}
+Minute: ${minute}
+Seconds: ${seconds}
+Day Of Week: ${day_of_week} [${day_of_week_int}]
+Formatted Date: ${formatted_date}
+"
+echo "${date_details}"
+
